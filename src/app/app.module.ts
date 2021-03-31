0d0ac60af12ee5b26f7e3f2511dfacd4a37d1ca0
Subhay Manandhar <subhaymanandhar@subhays-mbp.lan>
2021-03-30 21:04:48 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewFormComponent } from './new-form/new-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFormComponent
   ],
   imports: [
     BrowserModule
